5a086d87b45ec4f3a76688dd8c1af88bf7c4c58d
Vladimir Seregin <31631@rambler.ru>
2017-05-06 21:50:29 +0600

    added "CORS headers" middleware

--- taskTracker/settings/base.py
+++ taskTracker/settings/base.py
@@ -37,9 +37,12 @@ INSTALLED_APPS = [
     'tracker',          #app
     'taggit',           #tags for tasks
     'rest_framework',   #for API
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+#CORS header settings
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+'''CORS_ORIGIN_WHITELIST = (
+    '<YOUR_DOMAIN>[:PORT]',
+)
+'''
